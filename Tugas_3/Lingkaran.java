b5bd18a399ddddc5355169b78ff826f48d97bda4
Muhammad Al Kindy <92189267+Muhammad-Al-Kindy@users.noreply.github.com>
2022-09-18 23:31:37 +0700

    Add files via upload

---
+++ Tugas_3/Lingkaran.java
@@ -0,0 +1,14 @@
+package Tugas_3;
+
+public class Lingkaran {
+    public double phi;
+    public double r;
+
+    public double hitungLuas(){
+        return  r*r*phi;
+    }
+
+    public double hitungKeliling(){
+        return 2*r*phi;
+    }
+}
