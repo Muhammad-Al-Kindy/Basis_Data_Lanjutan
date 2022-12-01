b6298dd92a0eb73b42c8d705252466a2540935c4
Muhammad Al Kindy <92189267+Muhammad-Al-Kindy@users.noreply.github.com>
2022-12-01 19:08:00 +0700

    Delete AnakPemburu.java

--- Kuis2/src/Kuis2/AnakPemburu.java
+++
@@ -1,8 +0,0 @@
-package Kuis2;
-
-public class AnakPemburu extends Pemburu implements IMengambilTelur{
-    @Override
-    public void mengambilTelur(Dinosaurus dino) {
-        System.out.println("Anak pemburu mengambil telur dino");
-    }
-}
