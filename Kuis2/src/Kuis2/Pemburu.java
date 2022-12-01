5e5b4a36298452ea54652ec90552ed30d24faf19
Muhammad Al Kindy <92189267+Muhammad-Al-Kindy@users.noreply.github.com>
2022-12-01 19:08:39 +0700

    Delete Pemburu.java

--- Kuis2/src/Kuis2/Pemburu.java
+++
@@ -1,9 +0,0 @@
-package Kuis2;
-
-public class Pemburu {
-    public void berburu(TyrannosaurusRex trex){
-        System.out.println("Pemburu berburu TyrannosaurusRex yang memiliki ciri ciri ");
-        trex.makan();
-        trex.berjalan();
-    }
-}
